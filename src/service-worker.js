8e47bb5507afa8d61dab0cf62dab7b94c026fcf9
Jacky <jacky@JackydeMacBook-Pro.local>
2020-03-10 17:46:11 +0800

    update cache finished

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,20 +7,25 @@ var promises = []
 const api_base = "https://api.syncobox.com";
 
 
-self.addEventListener('install',event => {
+self.addEventListener('install', event => {
 
   shareCode = new URL(location).searchParams.get('shareCode');
   cacheName = 'syncobox_panorama_' + shareCode
+  let time = new Date().toString()
   console.log('service worker: ', shareCode)
 
-  fetch(`${api_base}/PanoramaPWA/${shareCode}`)
+  fetch(`${api_base}/PanoramaPWA/${shareCode}?${time}`)
     .then(async res => {
+      await caches.delete(cacheName)
       await caches.open(cacheName)
-        .then(async cache => await cache.put(`${api_base}/PanoramaPWA/${shareCode}`, res.clone()))
+        .then(async cache => {
+          await cache.put(`${api_base}/PanoramaPWA/${shareCode}`, res.clone())
+        })
       return res
     })
     .then(res => res.json())
     .then(data => preCache(data))
+ 
 })
 
 self.addEventListener('activate', function (event) {
@@ -28,7 +33,7 @@ self.addEventListener('activate', function (event) {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('message', function (event) {
+self.addEventListener('message', async function (event) {
   console.log('service worker on message')
   if (event.data && event.data.type === 'CLEAN_CACHE') {
     caches.delete(cacheName)
@@ -38,18 +43,27 @@ self.addEventListener('message', function (event) {
 
 
 self.addEventListener('fetch', function (event) {
-  // console.log('service worker on fetch')
-  // console.log('fetch: ' + event.request.url)
-  event.respondWith(
-    caches.open(cacheName).then(function (cache) {
-      return cache.match(event.request.url).then(function (response) {
-        return response || fetch(event.request).then(function (response) {
-          cache.put(event.request.url, response.clone());
-          return response;
+
+  if (navigator.onLine) {
+    event.respondWith(
+      caches.open(cacheName).then(function (cache) {
+        return cache.match(event.request.url).then(function (response) {
+          return response || fetch(event.request.url)
         });
-      });
-    })
-  )
+      })
+    )
+  }
+  else {
+    event.respondWith(
+      caches.open(cacheName).then(function (cache) {
+        return cache.match(event.request.url, { ignoreSearch: true }).then(function (response) {
+          return response
+        });
+      })
+    )
+  }
+
+
 })
 
 function preCache(resources) {
@@ -57,46 +71,42 @@ function preCache(resources) {
     .then(async cache => {
 
       // this page
-      await promises.push(cache.add(`/view/${shareCode}`))
+      promises.push(cache.add(`/view/${shareCode}`))
       console.log('page collected')
 
       // static files
       await self.__precacheManifest.forEach(async e => {
-        await promises.push(cache.add(e.url))
+        promises.push(cache.add(e.url))
       })
       console.log('static files collected')
 
       // all information about panorama
       await resources.panoramaIds.forEach(async id => {
-        await promises.push(
-          fetch(`${api_base}/Panorama/${id}`)
-            .then(async json => {
-              await cache.put(`${api_base}/Panorama/${id}`, json)
-              await cache.add(`${api_base}/Panorama/GetImage/${id}/pano_l.jpg`)
-              await cache.add(`${api_base}/Panorama/GetImage/${id}/pano_r.jpg`)
-              await cache.add(`${api_base}/Panorama/GetImage/${id}/pano_u.jpg`)
-              await cache.add(`${api_base}/Panorama/GetImage/${id}/pano_d.jpg`)
-              await cache.add(`${api_base}/Panorama/GetImage/${id}/pano_f.jpg`)
-              await cache.add(`${api_base}/Panorama/GetImage/${id}/pano_b.jpg`)
-              await cache.add(`${api_base}/Panorama/GetImage/${id}/preview.jpg`)
-              await cache.add(`${api_base}/Panorama/GetImage/${id}/thumb.jpg`)
-              await cache.add(`${api_base}/Panorama/GetImage/${id}/border_thumb_128.png`)
-            })
-            .catch((err) => {
-              console.log('錯誤:', err);
-            })
-        )
+        promises.push(fetch(`${api_base}/Panorama/${id}`)
+          .then(async (json) => {
+            await cache.put(`${api_base}/Panorama/${id}`, json);
+            await cache.add(`${api_base}/Panorama/GetImage/${id}/pano_l.jpg`);
+            await cache.add(`${api_base}/Panorama/GetImage/${id}/pano_r.jpg`);
+            await cache.add(`${api_base}/Panorama/GetImage/${id}/pano_u.jpg`);
+            await cache.add(`${api_base}/Panorama/GetImage/${id}/pano_d.jpg`);
+            await cache.add(`${api_base}/Panorama/GetImage/${id}/pano_f.jpg`);
+            await cache.add(`${api_base}/Panorama/GetImage/${id}/pano_b.jpg`);
+            await cache.add(`${api_base}/Panorama/GetImage/${id}/preview.jpg`);
+            await cache.add(`${api_base}/Panorama/GetImage/${id}/thumb.jpg`);
+            await cache.add(`${api_base}/Panorama/GetImage/${id}/border_thumb_128.png`);
+          })
+          .catch((err) => {
+            console.log('錯誤:', err);
+          }))
       })
       console.log('pano collected')
 
       // map images
       await resources.mapIds.forEach(async id => {
-        await promises.push(
-          fetch(`${api_base}/PanoramaMap/GetImage/${id}`)
-            .then(async res => {
-              await cache.put(`${api_base}/PanoramaMap/GetImage/${id}`, res)
-            })
-        )
+        promises.push(fetch(`${api_base}/PanoramaMap/GetImage/${id}`)
+          .then(async (res) => {
+            await cache.put(`${api_base}/PanoramaMap/GetImage/${id}`, res);
+          }))
       })
       console.log('map collected')
       Promise.all(promises).then(e => self.clients.matchAll().then(all => all.map(client => client.postMessage({ type: 'PRECACHE_DONE' }))))
--- src/views/PanoViewPage.vue
+++ src/views/PanoViewPage.vue
@@ -191,7 +191,23 @@ export default {
     manageInstallPrompt() {
       window.addEventListener("beforeinstallprompt", function(e) {
         e.preventDefault();
-      })
+      });
+    },
+
+    sendMessage(message) {
+      return new Promise(function(resolve, reject) {
+        var messageChannel = new MessageChannel();
+        messageChannel.port1.onmessage = function(event) {
+          if (event.data.error) {
+            reject(event.data.error);
+          } else {
+            resolve(event.data);
+          }
+        };
+        navigator.serviceWorker.controller.postMessage(message, [
+          messageChannel.port2
+        ]);
+      });
     }
   },
   watch: {
