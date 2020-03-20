911ddf037960b7d368ae4f787cd376729aea3efd
Jacky <jacky@JackydeMacBook-Pro.local>
2020-03-20 14:28:45 +0800

    add GA

--- src/main.js
+++ src/main.js
@@ -3,8 +3,23 @@ import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
 import KrpanoVue from 'krpano-vue'
+import VueAnalytics from 'vue-analytics'
+
+var production = process.env.NODE_ENV == 'production'
 
 Vue.use(KrpanoVue);
+
+Vue.use(VueAnalytics, {
+  id: 'UA-161417188-1',
+  router,
+  debug: {
+    enabled: !production,
+    sendHitTask: production
+  },
+  autoTracking: {
+    pageviewOnLoad: false
+  }
+})
 
 Vue.config.productionTip = false
 
